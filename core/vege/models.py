a2e0e1f94f0dd3a7b1e2c5695b8433aaadd3748f
sonko <sonko@debian.roy>
2023-11-07 10:46:44 +0530

    add view_count

--- core/vege/models.py
+++ core/vege/models.py
@@ -6,5 +6,6 @@ class Receipe(models.Model):
     receipe_name = models.CharField(max_length=100)
     receipe_description = models.TextField()
     receipe_image = models.ImageField(upload_to="receipe")
+    receipe_view_count = models.IntegerField(default=1)
 
 
