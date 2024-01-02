904b1b0f2a96aaa09e82eb4b7062cc610d97c80c
sankha <sankhadeep41@gmail,com>
2024-01-02 21:03:12 +0530

    search

--- core/vege/views.py
+++ core/vege/views.py
@@ -127,14 +127,24 @@ def register_page(request):
     
 
 
+from django.db.models import Q
 
 def get_students(request):
     queryset = Student.objects.all()
+    
+    if request.GET.get('search'):
+        search = request.GET.get('search')
+        queryset = queryset.filter(
+            Q(student_name__icontains = search) |
+            Q(department__department__icontains = search) |
+            Q(student_id__student_id__icontains = search) 
+            )
+    
     paginator = Paginator(queryset, 3)  # Show 25 contacts per page.
 
     page_number = request.GET.get("page",1)
     page_obj = paginator.get_page(page_number)
 
-    return render(request , 'report/students.html' , {'queryset' : page_obj} )
+    return render(request , 'report/students.html' , {'queryset' : page_obj})
 
 
